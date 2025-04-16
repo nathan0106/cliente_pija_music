5fe36e9c1e83d5ec2dc70a7e4d2f135cc9ebee21
luisafernada08 <luisagoyeneche09@gmail.com>
2025-04-15 20:23:44 -0500

    corriguindo errores

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/usuario/registro/login/login.component';
 import { ContrasenaComponent } from './pages/usuario/registro/contrasena/contrasena.component';
+import { InstrumentosComponent } from './pages/usuario/cultura/instrumentos/instrumentos.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'contraseña', component: ContrasenaComponent},
+  {path:'Instrumentos',component:InstrumentosComponent},
 ];
 
 @NgModule({
