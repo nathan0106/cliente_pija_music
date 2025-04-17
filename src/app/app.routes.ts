108d288c33a51460352d19982b8ddf33157eca93
KatherineRincon <ingry_krincon@soy.sena.edu.co>
2025-04-17 09:24:31 -0500

    trabajando en el componente de senderismo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/usuario/registro/login/login.component';
 import { ContrasenaComponent } from './pages/usuario/registro/contrasena/contrasena.component';
+import { SenderismoComponent } from './pages/usuario/turismo/senderismo/senderismo.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'contraseña', component: ContrasenaComponent},
+  { path: 'senderismo', component: SenderismoComponent},
+  
 ];
 
 @NgModule({
--- src/app/pages/usuario/turismo/senderismo/senderismo.component.ts
+++ src/app/pages/usuario/turismo/senderismo/senderismo.component.ts
@@ -1,11 +1,49 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'; 
 
 @Component({
   selector: 'app-senderismo',
-  imports: [],
+  standalone:true,
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
   templateUrl: './senderismo.component.html',
   styleUrl: './senderismo.component.css'
 })
 export class SenderismoComponent {
 
+  lugares = [
+    {
+      nombre: 'Mirador de la virgen de manare',
+      ubicacion: 'Yopal, Casanare',
+      distancia: '2.0 km',
+      imagen: 'assets/senderismo1.jpg'
+    },
+    {
+      nombre: 'Balcones del Cacique',
+      ubicacion: 'Tauramena, Casanare',
+      distancia: '10 km',
+      imagen: 'assets/senderismo2.jpg'
+    },
+    {
+      nombre: 'Parque temático Historia de piedra',
+      ubicacion: 'Yopal, Casanare',
+      distancia: '5.5 km',
+      imagen: 'assets/senderismo3.jpg'
+    }
+  ];
 }
