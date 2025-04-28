f0c08cf0a42cee78d4a95d5dab316dc521c374be
MafeRinaldis <rinaldismafe09@gmail.com>
2025-04-28 17:05:55 -0500

    modificaciones en menu principal nuevamente, en favoritos y en  otricas

--- src/app/pages/usuario/musica/favoritos/favoritos.component.ts
+++ src/app/pages/usuario/musica/favoritos/favoritos.component.ts
@@ -1,18 +1,33 @@
 
 import { Component, Input } from '@angular/core';
-import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { CommonModule } from '@angular/common';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'; 
+import { Router } from '@angular/router';
 import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-favoritos',
+  standalone:true,
   imports: [
     MatCardModule,
     MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
     CommonModule,
     RouterModule
+
   ],
   templateUrl: './favoritos.component.html',
   styleUrl: './favoritos.component.css'
@@ -29,6 +44,10 @@ export class FavoritosComponent {
     { title: 'Jhon Onofre', imageUrl: 'Fav6.png' },
     { title: 'Jorge Calderón', imageUrl: 'Fav7.png' },
     { title: 'Yaguazo', imageUrl: 'Fav8.png' },
+    { title: 'Yaguazo', imageUrl: 'Art1.png' },
+    { title: 'Walter Silva', imageUrl: 'Art2.png' },
+    { title: 'Ronald Arrieta', imageUrl: 'Art3.png' },
+    { title: 'Mateo Benítez', imageUrl: 'Art4.png' },
   ];
 
   constructor(private router: Router) {}
--- src/app/pages/usuario/musica/playlist/playlist.component.ts
+++ src/app/pages/usuario/musica/playlist/playlist.component.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'; 
 
 @Component({
-  selector: 'app-playlist',
-  imports: [],
+  selector: 'app-notificaciones',
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    CommonModule,
+
+  ],
   templateUrl: './playlist.component.html',
   styleUrl: './playlist.component.css'
 })
