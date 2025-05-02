a5973a7e49af57eb19d749d46e91aadf946e1666
KatherineRincon <ingry_krincon@soy.sena.edu.co>
2025-05-02 14:56:23 -0500

    hice algunos cambios en los componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/usuario/registro/login/login.component';
 import { ContrasenaComponent } from './pages/usuario/registro/contrasena/contrasena.component';
 import { SenderismoComponent } from './pages/usuario/turismo/senderismo/senderismo.component';
+import { RegisterComponent } from './pages/usuario/registro/register/register.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: 'contraseña', component: ContrasenaComponent},
   { path: 'senderismo', component: SenderismoComponent},
   
