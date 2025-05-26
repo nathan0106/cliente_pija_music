3c5a96e228e1ca6903fd8cf8ff7afac0cfc62578
MafeRinaldis <rinaldismafe09@gmail.com>
2025-05-26 16:52:22 -0500

    modificaciones en canciones administrador

--- src/app/pages/administrador/cancionesadministrador/cancionesadministrador.component.ts
+++ src/app/pages/administrador/cancionesadministrador/cancionesadministrador.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ApiService } from '../../../services/api.service';
 
 @Component({
   selector: 'app-cancionesadministrador',
@@ -10,10 +11,13 @@ import { FormsModule } from '@angular/forms';
     CommonModule  
   ],
   templateUrl: './cancionesadministrador.component.html',
-  styleUrl: './cancionesadministrador.component.css'
+  styleUrls: ['./cancionesadministrador.component.css']
 })
 export class CancionesadministradorComponent {
-  artistas = ['Jorge Albarracín', 'Jorge Guerrero', 'Cristina Maica']; // Puedes cargar dinámicamente
+   constructor(private apiService: ApiService) {}
+  artistas: any[]=[]; // Puedes cargar dinámicamente
+
+  estilos: any[] = [];
 
   canciones: any[] = [];
 
@@ -27,8 +31,52 @@ export class CancionesadministradorComponent {
     fechaLanzamiento: ''
   };
 
+  cancionjsnpost={
+    TituloCancion:'',
+    IdArtistas :{
+      Id:0
+       },
+    Album:'',
+    IdEstilo:{
+      Id:0
+    },
+    Duracion:'',
+    RutaArchivo:'',
+    FechaLanzamiento:'',
+  }
+
+
   guardarCancion() {
     this.canciones.push({ ...this.cancionNueva });
+    this.cancionjsnpost={
+      TituloCancion:this.cancionNueva.titulo,
+      IdArtistas:{
+        Id: Number(this.cancionNueva.artista)
+      },
+      Album:this.cancionNueva.album,
+      IdEstilo: {
+        Id: Number(this.cancionNueva.idEstilo)
+      },
+      Duracion:this.cancionNueva.duracion,
+      RutaArchivo:this.cancionNueva.link,
+      FechaLanzamiento:this.cancionNueva.fechaLanzamiento
+    }
+
+     let cancionNueva_json = JSON.stringify(this.cancionjsnpost);
+
+     console.log('json',cancionNueva_json)
+
+    this.apiService.postData('Canciones', cancionNueva_json).subscribe(
+  (respuesta) => {
+    console.log('cancion creado', respuesta);
+    alert('cancion musical creado exitosamente');
+  },
+  (error) => {
+  console.error('Error al crear la cancion ', error);
+  alert('Error al crear la cancion');
+ }
+);
+
     this.cancionNueva = {
       titulo: '',
       duracion: '',
@@ -39,4 +87,34 @@ export class CancionesadministradorComponent {
       fechaLanzamiento: ''
     };
   }
+
+  ngOnInit(): void {
+    // Este código se ejecuta después de que Angular ha inicializado el componente
+    console.log('Componente inicializado');
+
+    this.apiService.getData('Artista?limit=0').subscribe(
+    (respuesta) => {
+      console.log('Artistas cargados', respuesta);
+      this.artistas= respuesta["Data"]
+    },
+    (error) => {
+    console.error('Error al consultar los artistas', error);
+    alert('Error al consultar Aristas');
+  }
+  );
+
+    
+  this.apiService.getData('Estilo_Musical?limit=0').subscribe(
+  (respuesta) => {
+  console.log('Estilos cargados', respuesta);
+  this.estilos = respuesta["Data"];
+  },
+  (error) => {
+  console.error('Error al consultar estilos musicales', error);
+  alert('Error al consultar estilos musicales');
+  }
+);
 }
+
+  }
+
